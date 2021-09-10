115f36e55d1c22378a87234758b717522ebf5bfa
jwigl631 <jwigl631@evit.com>
2021-09-10 12:49:04 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,29 +5,34 @@ from tkinter import *
 import random
 root = Tk()
 root.title('Tic Tac Toe')
-root.geometry(500, 500)
 
 turns = 0
-buttonlist = []
+buttons = []
 size = 0
+quantity = StringVar()
 
-def board(size, buttonlist,number):
+def mainMenu():
+    clearWindow()
+    Button(root, text="Singleplayer").grid(row=0, column=0)
+    Button(root, text="Multiplayer").grid(row=0, column=0)
+
+def start():
+    Button(root, command=lambda: board(size, buttons, quantity), text="Enter").grid(row=0, column=0)
+    Entry(root, textvariable=quantity).grid(row=1, column=0)
+
+def board(size, buttons,number):
     clearWindow()
     number = int(number.get())
-    buttonlist = [[] for i in range(number)]
+    buttons = [[] for i in range(number)]
     for row in range(number):
         for column in range(number):
-            buttonlist[row].append(Button(root,text='-',width=12, height=4))
-            buttonlist[row][column]
-            buttonlist[row][column].grid(row=row+1,column=column)
-
-number = StringVar()
-sim = Button(root, command=lambda: board(size,buttonlist,number),text="Submit").grid(row=0,column=0)
-label = Entry(root, textvariable=number).grid(row=1,column=0)
+            buttons[row].append(Button(root,text='-',width=12, height=4))
+            buttons[row][column]
+            buttons[row][column].grid(row=row+1,column=column)
 
 def clearWindow():
     for widget in root.winfo_children():
         widget.destroy()
 
-
+start()
 root.mainloop()
