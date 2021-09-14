9b64cfbf6c08c7a004171ed3da4a0cc315452aff
jwigl631 <jwigl631@evit.com>
2021-09-14 13:33:15 -0700

    Added comments, refined the board check making it less error prone as well as more organized and fixed the board to display the current turn when clicked

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ root.title('Tic Tac Toe')
 global gameBoard
 turns = 0
 boardButtons = []
-systemButtons = []
 quantity = StringVar()
 mode = ""
 turn = "X"
 
+#Main menu, choose between single player and multiplayer
 def mainMenu():
     clearWindow()
     Label(root, text="Welcome To Tic Tac Toe", font='Arial').grid(row=0, column=0)
@@ -22,44 +22,50 @@ def mainMenu():
     Button(root, text="Singleplayer", height=3, width=10, command=boardStart).grid(row=2, column=0)
     Button(root, text="Multiplayer", height=3, width=10, command=boardStart).grid(row=3, column=0)
 
+#Where the player inputs the size of the board
 def boardStart():
     clearWindow()
-    Label(root, text="Enter the board size (3-9)", font='Arial').grid(row=0, column=1)
+    Label(root, text="Enter the board size (Odd between 3-9)", font='Arial').grid(row=0, column=1)
     Entry(root, textvariable=quantity).grid(row=1, column=1)
-    Button(root, command=lambda: board(quantity.get()), text="Enter", height=2, width=10).grid(row=2, column=1)
+    Button(root, command=lambda: check(quantity.get()), text="Enter", height=2, width=10).grid(row=2, column=1)
     Button(root, text="Return", command=mainMenu, height=2, width=10).grid(row=3, column=1)
 
-def board(quantity):
-    global gameBoard
-    clearWindow()
+#Makes sure the quantity is an odd number between 3 and 9
+def check(quantity):
     quantity1 = int(quantity)
-    systemBoard(quantity1)
-
-    if quantity1 % 3 == 0 and quantity1 > 0 and quantity1 < 10:
-        boardButtons = [[] for i in range(quantity1)]
-        for row in range(quantity1):
-            for column in range(quantity1):
-                boardButtons[row].append(Button(root,text=gameBoard[row][column], width=12, height=4))
-                boardButtons[row][column].config(command=lambda currentrow = row, currentcolumn = column: click(currentrow, currentcolumn, quantity1))
-                boardButtons[row][column].grid(row=row+1,column=column)
-        if mode == "m":
-            print("multi")
-        elif mode == "s":
-            print("single")
+    if quantity1 % 2 == 1 and quantity1 > 1 and quantity1 < 10:
+        createBoard(quantity1)
     else:
-        messagebox.showinfo("Error", "Input a number that is a multiple of 3 between 3-9")
+        messagebox.showinfo('Error', 'Input an odd value (3-9)')
         boardStart()
 
-def systemBoard(quantity1):
+#Creates the game board arrays that store the X/O values, call only once
+def createBoard(quantity1):
     global gameBoard
-    gameBoard = [["" for j in range(quantity1)] for i in range(quantity1)]
+    print(quantity1)
+    gameBoard = [["-" for j in range(quantity1)] for i in range(quantity1)]
     print(gameBoard)
-
-def click(row, column, quantity1):
-    global gameBoard
-    gameBoard[row][column] = 'x'
     board(quantity1)
 
+#Displays the buttons for the boards and updates them
+def board(quantity1):
+    global gameBoard
+    clearWindow()
+    boardButtons = [[] for i in range(quantity1)]
+    for row in range(quantity1):
+        for column in range(quantity1):
+            boardButtons[row].append(Button(root,text=gameBoard[row][column], width=12, height=4))
+            boardButtons[row][column].config(command=lambda currentrow = row, currentcolumn = column: click(currentrow, currentcolumn, quantity1))
+            boardButtons[row][column].grid(row=row+1,column=column)
+
+#Function that changes the button based on which one it clicks
+def click(row, column, quantity1):
+    global gameBoard
+    gameBoard[row][column] = 'X'
+    print(gameBoard)
+    board(quantity1)
+
+#Clear the window
 def clearWindow():
     for widget in root.winfo_children():
         widget.destroy()
